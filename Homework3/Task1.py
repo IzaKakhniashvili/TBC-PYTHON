e2b304cb3f4ee3430f858dee17cc85c7c39440a9
Iza Kakhniashvili <152190428+IzaKakhniashvili@users.noreply.github.com>
2024-03-09 20:40:01 +0400

    Update Task1.py

--- Homework3/Task1.py
+++ Homework3/Task1.py
@@ -4,7 +4,7 @@ import math
 a = int(input("Enter number: "))
 b = int(input("Enter the power of " + a + " : "))
 
-if a > 0:
+if b > 0:
     c = math.pow(a, b)
     print(a + " raised to the power b: " + c )
 else:
