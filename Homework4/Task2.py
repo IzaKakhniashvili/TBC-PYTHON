75aa4a8d2d7260d026f22c0dba8b4ac014393517
Iza Kakhniashvili <152190428+IzaKakhniashvili@users.noreply.github.com>
2024-03-16 20:14:40 +0400

    Update Task2.py

--- Homework4/Task2.py
+++ Homework4/Task2.py
@@ -3,16 +3,16 @@ import random
 
 n = (input("Enter n: "))
 
-max = 0
+_max = 0
 if n.isdigit(): #will check if input is integer.
     n = int(n) #will cast str n to int n.
     if n > 0  and n < 30 : 
         for i in range(n):
             a = random.randint(0, 1000)
             print(a)
-            if a > max:
-                max = a
-        print(f"Max is: {max}")
+            if a > _max:
+                _max = a
+        print(f"Max is: {_max}")
     else:
         print("Please enter a value of n between 1 and 30.")
 else:
