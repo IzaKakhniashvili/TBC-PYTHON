bafc76a1dbd7537d15515c662e1978238c36ee78
Iza Kakhniashvili <izakakhniashvili@192.168.0.100>
2024-03-16 19:52:54 +0400

    Homework 7

---
+++ Homework7/Task3.py
@@ -0,0 +1,13 @@
+n = int(input("Enter number: "))
+
+while not(n > 0  and n <= 10000):
+    n = int(input("Number should be between 0 and 10000: "))
+
+n_reversed = 0
+
+while n != 0:
+    r = n % 10
+    n_reversed = n_reversed * 10 + r
+    n = n // 10 
+    
+print(n_reversed)
