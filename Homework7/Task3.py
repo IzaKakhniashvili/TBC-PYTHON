88863a88aaf5dc255ea0b4558575d4b68f1332b9
Iza Kakhniashvili <izakakhniashvili@192.168.0.101>
2024-03-18 20:25:52 +0400

    Update Homework7

--- Homework7/Task3.py
+++ Homework7/Task3.py
@@ -2,13 +2,20 @@ n = int(input("Enter number: "))
 
 while not(n > 0  and n <= 10000):
     n = int(input("Number should be between 0 and 10000: "))
-
+n_sum = n
 n_reversed = 0
 
+print("Reversed number is: ", end = "")
 while n != 0:
     r = n % 10
-    n_reversed = n_reversed * 10 + r
+    print(r, end = "")
     n = n // 10 
-    
-print(n_reversed)
+print() 
 
+sum_of_digits = 0
+while n_sum != 0:
+    r = n_sum % 10
+    sum_of_digits += r
+    n_sum = n_sum // 10
+    
+print(f"Sum of digits: {sum_of_digits}")
