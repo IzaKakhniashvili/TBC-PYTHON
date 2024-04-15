471a6fd8abb903d85fb9be8f48b620c0555fc87e
Iza Kakhniashvili <izakakhniashvili@192.168.0.101>
2024-04-15 20:58:23 +0400

    Task3

--- Midterm/Task3.py
+++ Midterm/Task3.py
@@ -8,15 +8,15 @@ def generate_rps():
     return comp[n]
 
 
-    
-comp_rps = generate_rps()
-user_rps = input("Rock, Paper, Scissor. Enter R/P/S: ")
-print(f"Computer RPS: {comp_rps}.")
-res = comp_rps + user_rps
+def main():    
+    comp_rps = generate_rps()
+    user_rps = input("Rock, Paper, Scissor. Enter R/P/S: ")
+    print(f"Computer RPS: {comp_rps}.")
+    res = comp_rps + user_rps
 
-if res == "RS" or res == "SP" or res == "PR":
-    print("Computer is the winner.")
-elif res == "SR" or res == "PS" or res == "RP":
-    print("User is the winner.")
-else:
-    print("Nobody is the winner.")
+    if res == "RS" or res == "SP" or res == "PR":
+        print("Computer is the winner.")
+    elif res == "SR" or res == "PS" or res == "RP":
+        print("User is the winner.")
+    else:
+        print("Nobody is the winner.")
